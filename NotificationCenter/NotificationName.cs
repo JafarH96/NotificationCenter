bdfa0a4a704190204e21e84abc47f219114dd453
Jafar <jafarheidary75@gmail.com>
2023-02-09 18:45:43 +0330

    First Commit

---
+++ NotificationCenter/NotificationName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationCenter
+{
+    public class NotificationName
+    {
+        public string name { get; }
+        public NotificationName(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
